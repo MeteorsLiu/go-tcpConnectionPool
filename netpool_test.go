2e6e84491aa347662a2b40402a028471f2b2ff28
GuoHaoLan <coopticks@hotmail.com>
2022-09-08 20:38:34 +0800

    Put close test

---
+++ netpool_test.go
@@ -0,0 +1,32 @@
+package connpool
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"testing"
+)
+
+func serve() {
+	l, _ := net.Listen("tcp", "127.0.0.1:9999")
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			t.Log(err)
+			continue
+		}
+		go func() {
+			defer c.Close()
+			res, _ := io.ReadAll(c)
+			fmt.Println(res)
+		}()
+	}
+}
+
+func TestNetconn(t *testing.T) {
+	go serve()
+	w := Wrapper(New("127.0.0.1:9999", DefaultOpts()))
+	for i := 0; i < 10000; i++ {
+		go w.Write([]byte(fmt.Sprintf("Test%d", i)))
+	}
+}
