323f45d9ec55ce7b1e27d6cd8cbac7abc4f80669
GuoHaoLan <coopticks@hotmail.com>
2022-09-08 20:49:57 +0800

    Put close test

--- netconn_test.go
+++ netconn_test.go
@@ -2,36 +2,13 @@ package connpool
 
 import (
 	"fmt"
-	"io"
-	"net"
 	"sync"
 	"testing"
 )
 
-func serve(t *testing.T) {
-	l, err := net.Listen("tcp", "127.0.0.1:9998")
-	if err != nil {
-		t.Errorf("%v", err)
-		return
-	}
-	for {
-		c, err := l.Accept()
-		if err != nil {
-			t.Log(err)
-			continue
-		}
-		go func() {
-			defer c.Close()
-			res, _ := io.ReadAll(c)
-			t.Log(string(res))
-		}()
-	}
-}
-
 func TestNetconn(t *testing.T) {
 	var wg sync.WaitGroup
-	t.Parallel()
-	go serve(t)
+
 	w := Wrapper(New("127.0.0.1:9998", DefaultOpts()))
 
 	for i := 0; i < 10000; i++ {
