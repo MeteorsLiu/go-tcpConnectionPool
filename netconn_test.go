0421cb4441287c453c2c54667bf4858da0c3e034
root <coopticks@outlook.com>
2022-09-09 15:50:33 +0800

    Fix netconn_test.go

--- netconn_test.go
+++ netconn_test.go
@@ -11,16 +11,18 @@ func TestNetconn(t *testing.T) {
 	p := New("127.0.0.1:9998", DefaultOpts())
 	w := Wrapper(p)
 	defer w.Close()
-	for i := 0; i < 10000; i++ {
+	id := make(chan int)
+	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			b := make([]byte, 2)
-			binary.LittleEndian.PutUint16(b, uint16(i))
+			binary.LittleEndian.PutUint16(b, uint16(<-id))
 			if _, err := w.Write(b); err != nil {
 				t.Log(err)
 			}
 		}()
+		id <- i
 		t.Log(p.Len())
 	}
 	wg.Wait()
