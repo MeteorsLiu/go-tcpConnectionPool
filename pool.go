504d239d289260f93dcbd47b073e7fedb4478514
GuoHaoLan <coopticks@hotmail.com>
2022-10-23 16:12:01 +0800

    Add some methods

--- pool.go
+++ pool.go
@@ -426,6 +426,7 @@ func (p *Pool) Put(c *ConnNode) {
 
 // close all connection.
 func (p *Pool) Close() {
+	log.Printf("Epoll len: %d, len: %d", p.epoll.len, p.len)
 	p.EpollClose()
 	node := p.head
 	for node != nil {
