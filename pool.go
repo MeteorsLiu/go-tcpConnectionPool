cd04bcfb065a03872aec702e0e749902d85eee01
GuoHaoLan <coopticks@hotmail.com>
2022-11-08 16:00:36 +0800

    Better reading

--- pool.go
+++ pool.go
@@ -85,7 +85,7 @@ func (p *Pool) epollInit() error {
 // epoll event add
 func (p *Pool) eventAdd(fd int32) error {
 	var event syscall.EpollEvent
-	event.Events = syscall.EPOLLIN | syscall.EPOLLRDHUP
+	event.Events = syscall.EPOLLIN | syscall.EPOLLRDHUP | EPOLLET
 	event.Fd = fd
 	if err := syscall.EpollCtl(p.epoll.fd, syscall.EPOLL_CTL_ADD, int(fd), &event); err != nil {
 		return err
@@ -427,9 +427,7 @@ func (p *Pool) markReadable(n int) {
 
 			}
 		}
-		p.mutex.RLock()
 		node = node.next
-		p.mutex.RUnlock()
 	}
 	if hasBad {
 		log.Println("Some connections are disconnected. Try to reconnect...")
