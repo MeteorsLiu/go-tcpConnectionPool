584c3e9fa283974b914549825e342e590c6c391e
GuoHaoLan <coopticks@hotmail.com>
2022-11-08 16:10:28 +0800

    Better reading

--- pool.go
+++ pool.go
@@ -85,7 +85,7 @@ func (p *Pool) epollInit() error {
 // epoll event add
 func (p *Pool) eventAdd(fd int32) error {
 	var event syscall.EpollEvent
-	event.Events = syscall.EPOLLIN | syscall.EPOLLRDHUP | EPOLLET
+	event.Events = syscall.EPOLLIN | syscall.EPOLLRDHUP
 	event.Fd = fd
 	if err := syscall.EpollCtl(p.epoll.fd, syscall.EPOLL_CTL_ADD, int(fd), &event); err != nil {
 		return err
