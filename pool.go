011ebb6b7919152012b4a7fd640234f808e64342
GuoHaoLan <coopticks@hotmail.com>
2022-11-08 15:47:20 +0800

    Better reading

--- pool.go
+++ pool.go
@@ -372,9 +372,6 @@ func (p *Pool) Get() (*ConnNode, error) {
 // put the writable connection into the pool.
 func (p *Pool) Put(c *ConnNode) {
 	c.Lock.Unlock()
-	if p.head != c {
-		p.MoveToHead(c)
-	}
 }
 
 // close all connection.
